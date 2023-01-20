a5e4d1e32dcf467b153c6249cdc933bb9e5a3498
Rinava <asdasd5056@gmail.com>
2023-01-20 00:56:12 -0300

    search input styling

--- src/components/Auctions/components/Search/index.tsx
+++ src/components/Auctions/components/Search/index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { AuctionsContext } from '../..';
 import { Input } from '@/components/commons/Forms';
+import styles from './styles.module.css';
+import clsx from 'clsx';
 
 interface SearchProps {
   className?: string;
@@ -10,7 +12,7 @@ const Search = ({ className }: SearchProps) => {
 
   return (
     <Input
-      className={className}
+      className={clsx(styles.search, className)}
       placeholder='Type your keywords'
       onChange={(value) => setFilters({ type: 'search', payload: value })}
       value={filters.search}
